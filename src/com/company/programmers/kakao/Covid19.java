a4e2c56f564d7354afed512218f7f76b07c0fc02
JihoKevin <jhshin925@gmail.com>
2021-10-19 10:25:31 +0900

    covid-19 kakao

--- src/com/company/programmers/kakao/Covid19.java
+++ src/com/company/programmers/kakao/Covid19.java
@@ -7,4 +7,24 @@ package com.company.programmers.kakao;
  * Time: 12:17 오후
  */
 public class Covid19 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int[5];
+        for (int i = 0; i < places.length; i++) {
+            String[][] cusTable = new String[5][5];
+            for (int j = 0; j < places[i].length; j++) {
+                //System.out.println(places[i].length);
+                cusTable[i] = places[i][j].split("");
+                for (int k = 0; k < 5; k++) { // 여기가 각 문자열의 요소 반복문
+                    // cusTable[i][k];
+                }
+            }
+        }
+
+        return answer;
+    }
 }
