390424917aafdba9be04d4ea9fe4abdd7627dc01
JihoKevin <jhshin925@gmail.com>
2021-11-04 20:25:32 +0900

    TakePicture wip

--- src/com/company/programmers/kakao/TakePicture.java
+++ src/com/company/programmers/kakao/TakePicture.java
@@ -6,127 +6,123 @@ package com.company.programmers.kakao;
  * Date: 2021/11/04
  * Time: 9:42 오전
  */
-public class TakePicture {
+//public class TakePicture {
+//   public static int totalCnt = 0;
+//    private static String[] kakaoExample = {"A", "C", "F", "J", "M", "N", "R", "T"};
+//    private static String[] data1 = {"N~F=0", "R~T>2"};
+//
+//    String[] data2 = {"M~C<2", "C~M>1"};
+//    String[] data3 = {"A~B<2", "B~A>1"};
+//
+//    public static void main(String[] args) {
+//
+//        //String[] data1 = {"N~F=0", "R~T>2"};
+//        String[] data2 = {"M~C<2", "C~M>1"};
+//        String[] data3 = {"A~B<2", "B~A>1"};
+//
+//        solution(2, data1);
+//    }
+//
+//    public static int solution(int n, String[] data) {
+//       boolean[] visited = new boolean[kakaoExample.length];
+//
+//        dfs("", data, visited);
+//        int answer = totalCnt;
+//        System.out.println(answer);
+//        return answer;
+//    }
+//
+//    public static void dfs(String searchExample, String[] datas, boolean[] visited) {
+//        if (searchExample.length() == 7) {
+//            if (checkFriends(searchExample, datas)) {
+//                totalCnt++;
+//                System.out.println(totalCnt);
+//            }
+//            return;
+//        }
+//
+//        for (int i = 0; i < 8; i++) {
+//            if (!visited[i]) {
+//                visited[i] = true;
+//                searchExample = searchExample + kakaoExample[i];
+//                dfs(searchExample, datas, visited);
+//                visited[i] = false;
+//            }
+//        }
+//    }
+//
+//    public static boolean checkFriends(String searchExample, String[] datas) {
+//        for (String data : datas) {
+//            int sugPos = searchExample.indexOf(data.charAt(0));
+//            int othPos = searchExample.indexOf(data.charAt(2));
+//           // int resultPos = Math.abs(sugPos - othPos) + 1;
+//            char op = data.charAt(3); // 수식
+//            int index = data.charAt(4) -'0'; // 간격
+//            if (op == '=') {
+//                if (!(Math.abs(sugPos - othPos) == index+1)) return false; //둘 포지션 차이를 구하기 위해선 index+1 을 해야함에 주의
+//            } else if (op == '>') {
+//                if (!(Math.abs(sugPos - othPos) > index+1)) return false;
+//            } else if (op == '<') {
+//                if (!(Math.abs(sugPos - othPos) < index+1)) return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
 
-    static String[] kakaoExample = {"B", "A", "C"};
-    //static String[] kakaoExample = {"A", "C", "F", "J", "M", "N", "R", "T"};
-    //static boolean[] visitedExample = new boolean[kakaoExample.length];
-    static String[] searchExample = new String[kakaoExample.length];
-    static boolean[] visited = new boolean[kakaoExample.length];
-    //static String[] search = new String[kakao.length];
-    //static boolean bfsFlag = false;
-    static int totalCnt = 0;
+public class TakePicture {
+    public static int totalCnt = 0;
+    public static String[] kakaoExample = {"A", "C", "F", "J", "M", "N", "R", "T"};
+
 
     public static void main(String[] args) {
-
         String[] data1 = {"N~F=0", "R~T>2"};
         String[] data2 = {"M~C<2", "C~M>1"};
         String[] data3 = {"A~B<2", "B~A>1"};
 
-        solution(2, data3);
+        solution(2, data1);
     }
 
     public static int solution(int n, String[] data) {
-
-        String[] opArr = {"=", "<", ">"};
-        // 연산자
-        String op = "";
-        int dfsCount = 0;
-        for (int i = 0; i < n; i++) {
-            // 제안자 분리
-            String sug = data[i].substring(0, 1);
-            // 연산자 분리
-            for (String operator : opArr) {
-                if (data[i].substring(3, 4).equals(operator)) {
-                    op = operator;
-                }
-            }
-            // 상대방 분리
-            String oth = data[i].substring(2, 3);
-            // 떨어져있기를 원하는 거리 분리
-            int distance = Integer.parseInt(data[i].substring(4));
-
-            if(dfsCount <= 0){
-                dfs(sug, oth, distance, 0, op, n);
-            }
-            dfsCount++;
-
-            //dfs(sug, oth, distance, 0, op, n);
-
-        }
-
+        boolean[] isVisited = new boolean[8];
+        dfs("", isVisited, data);
         int answer = totalCnt;
-        //System.out.println(answer);
+        System.out.println(answer);
         return answer;
     }
 
-    public static void dfs(String sug, String oth, int distance, int start, String op, int lengthData) {
-
-        int sugPos = 0;
-        int othPos = 0;
-        int resultPos = 0;
-
-        if (start == kakaoExample.length) {
-            int dfsCnt = 0;
-            for (int j = 0; j < searchExample.length; j++) {
-                System.out.print(searchExample[j]);
-                if (sug.equals(searchExample[j])) {
-                    sugPos = j;
-                }
-                if (oth.equals(searchExample[j])) {
-                    othPos = j;
-                }
-
-                resultPos = Math.abs(sugPos - othPos - 1);
-
-                if(op.equals("=")){
-                    if (distance == resultPos) {
-
-                        dfsCnt++;
-                       // System.out.println(dfsCnt + " ====");
-                    }
-                }
-
-                if(op.equals(">")){
-                    if (resultPos > distance) {
-
-                        dfsCnt++;
-                        //System.out.println(dfsCnt + " >>>>");
-                    }
-                }
-                if(op.equals("<")){
-                    if (resultPos < distance) {
-                        dfsCnt++;
-                        //System.out.println(dfsCnt + " <<<<");
-                    }
-                }
-            }
-
-            System.out.println();
-            System.out.println(lengthData + " " + dfsCnt);
-//            System.out.println(lengthData == dfsCnt);
-            if(lengthData == dfsCnt){
+    public static void dfs(String names, boolean[] isVisited, String[] datas) {
+        if (names.length() == kakaoExample.length - 1) {
+            if (check(names, datas)) {
                 totalCnt++;
             }
-            System.out.println(totalCnt);
-           // System.out.println();
-
-        } else {
-
-            for (int i = 0; i < kakaoExample.length; i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    searchExample[start] = kakaoExample[i];
-                    dfs(sug, oth, distance, start + 1, op, lengthData);
-                    visited[i] = false;
-                }
+            return;
+        }
+        for (int i = 0; i < 8; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                String name = names + kakaoExample[i];
+                dfs(name, isVisited, datas);
+                isVisited[i] = false;
             }
         }
     }
 
-    public static boolean checkFriends() {
-
+    public static boolean check(String searchExample, String[] datas) {
+        for (int i = 0; i < datas.length; i++) {
+            int position1 = searchExample.indexOf(datas[i].substring(0, 1));
+            int position2 = searchExample.indexOf(datas[i].substring(2, 3));
+            char op = datas[i].charAt(3);
+            int index = datas[i].charAt(4) - '0';
+            int resultPos = Math.abs(position1 - position2);
+            if (op == '=') {
+                if (!(resultPos == index + 1)) return false;
+            } else if (op == '>') {
+                if (!(resultPos > index + 1)) return false;
+            } else if (op == '<') {
+                if (!(resultPos < index + 1)) return false;
+            }
+        }
+        return true;
     }
-
-
 }
