daf0114e57deddc90673f97e7a6841bd1760bb8e
JihoKevin <jhshin925@gmail.com>
2021-11-21 16:11:33 +0900

    BOK2210Jin

--- src/com/company/freespace/Queue01.java
+++ src/com/company/freespace/Queue01.java
@@ -10,45 +10,35 @@ import com.company.search.day03.IntQueue;
  * desc: Queue 구현하기!
  */
 public class Queue01 {
-    int num;
+
     int front;
     int rear;
     int[] que;
     int max;
+    int num;
 
     public Queue01(int capacity) {
-        num = rear = front = 0;
-        max = capacity;
         try {
+
+            front = rear = 0;
+
+            max = capacity;
             que = new int[max];
+
         } catch (Exception e) {
             max = 0;
         }
     }
 
-    public int enque(int x) throws IntQueue.OverflowIntQueueException {
-        if (num >= max) {
-            throw new IntQueue.OverflowIntQueueException();
-        }
-        que[rear++] = x;
-        num++;
-        if (rear == max) {
-            rear = 0;
-        }
-        return x;
-    }
+//    public int deque() throws IntQueue.OverflowIntQueueException {
+//        if(num >= 0) {
+//            throw new IntQueue.OverflowIntQueueException();
+//        }
+//
+//        que[front++];
+//
+//
+//        return x;
+//    }
 
-    public int deque() throws IntQueue.EmptyIntQueueException {
-        if (num <= 0) {
-            throw new IntQueue.EmptyIntQueueException();
-        }
-        int x = que[front++];
-        num--;
-
-        if (front == max) {
-            front = 0;
-        }
-
-        return x;
-    }
 }
