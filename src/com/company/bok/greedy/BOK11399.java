062a70c3038e985e10809f6d7b001f420561c45d
JihoKevin <jhshin925@gmail.com>
2021-11-23 12:15:58 +0900

    Solve BOK11399

--- src/com/company/bok/greedy/BOK11399.java
+++ src/com/company/bok/greedy/BOK11399.java
@@ -1,5 +1,6 @@
 package com.company.bok.greedy;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,47 +11,71 @@ import java.util.Scanner;
  * Time: 9:58 오후
  */
 public class BOK11399 {
-    public static boolean[] isVisited;
-    public static int k = 0;
-    public static int[] index = new int[]{};
-
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
+        int sum = 0;
+        int result = 0;
 
         int N = scan.nextInt();
         int[] arr = new int[N];
-        isVisited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
+            arr[i] = scan.nextInt();
         }
-        dfs(0, arr);
-        Arrays.sort(index);
 
-        System.out.print(index[0]);
+        Arrays.sort(arr);
+
+        for (int j = 0; j < arr.length; j++) {
+            sum += arr[j];
+            result += sum;
+        }
+        System.out.println(result);
+
 
     }
-
-    public static void dfs(int depth, int[] arr) {
-        if (depth == arr.length) {
-            int sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                if (i > 0) {
-                    sum += arr[i - 1];
-                }
-            }
-            index[++k] = sum;
-            return;
-        }
-
-        for (int i = depth; i < arr.length; i++) {
-            if (!isVisited[i]) {
-                isVisited[i] = true;
-                dfs(depth + 1, arr);
-                isVisited[i] = false;
-            }
-        }
-    }
-
-
 }
+
+//public class BOK11399 {
+//    public static boolean[] isVisited;
+//    public static int k = 0;
+//    public static int[] index = new int[]{};
+//
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//
+//        int N = scan.nextInt();
+//        int[] arr = new int[N];
+//        isVisited = new boolean[N];
+//
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = i + 1;
+//        }
+//        dfs(0, arr);
+//
+//        System.out.print(index[0]);
+//
+//    }
+//
+//    public static void dfs(int depth, int[] arr) {
+//        if (depth == arr.length) {
+//            int sum = 0;
+//            for (int i = 1; i < arr.length; i++) {
+//
+//                sum += arr[i - 1];
+//
+//            }
+//            index[k++] = sum;
+//            return;
+//        }
+//
+//        for (int i = depth; i < arr.length; i++) {
+//            if (!isVisited[i]) {
+//                isVisited[i] = true;
+//                dfs(depth + 1, arr);
+//                isVisited[i] = false;
+//            }
+//        }
+//    }
+//}
+//
