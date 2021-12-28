211dd430fb7f3d0f4d389e44d6fc603b42c1ad1d
JihoKevin <jhshin925@gmail.com>
2021-12-28 23:41:08 +0900

    wip newsClustering

---
+++ src/com/company/programmers/kakao/Programmers_뉴스클러스터링.java
@@ -0,0 +1,49 @@
+package com.company.programmers.kakao;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by JihoKevin.
+ * User: sinjiho
+ * Date: 2021/12/27
+ * Time: 6:10 오후
+ */
+public class Programmers_뉴스클러스터링 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+
+        String firstStrArray = str1.toLowerCase();
+        String secondStrArray = str2.toLowerCase();
+
+        ArrayList<String> firstList = new ArrayList<>();
+        ArrayList<String> secondList = new ArrayList<>();
+
+        // 0, 1, 2, 3, 4, 5
+        for(int i = 0; i < str1.length(); i++) {
+
+            String str = firstStrArray + firstStrArray.charAt(i+1);
+
+
+//            if(i == str1.length() - 1) {
+//
+//            } else {
+//                firstStrArray.substring(i, i+2);
+//            }
+
+        }
+
+
+
+
+        return answer;
+    }
+
+
+}
