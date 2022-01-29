42686d4c6a95dbcf092a8be062e114ba8c68f848
JihoKevin <jhshin925@gmail.com>
2022-01-29 22:46:11 +0900

    test

--- src/com/company/techtaka/LinkedListNode.java
+++ src/com/company/techtaka/LinkedListNode.java
@@ -16,10 +16,11 @@ public class LinkedListNode {
         this.data = data;
     }
 
+
     public void addNode(int data) {
         LinkedListNode end = new LinkedListNode(data);
         LinkedListNode n = this;
-
+        System.out.println(n);
         while (n.next != null) {
             n = n.next;
         }
