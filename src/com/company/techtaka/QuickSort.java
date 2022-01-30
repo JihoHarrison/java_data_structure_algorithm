33d528e77fa0c3ca3d5805c914eab275fe0bb0bc
JihoKevin <jhshin925@gmail.com>
2022-01-30 13:34:19 +0900

    QuickSort Retry

--- src/com/company/techtaka/QuickSort.java
+++ src/com/company/techtaka/QuickSort.java
@@ -2,31 +2,36 @@ package com.company.techtaka;
 
 public class QuickSort {
 
-
     public static void main(String[] args) {
 
-
-        int[] array = {4, 3, 6, 2, 67, 2, 13, 5, 5, 2};
+        int[] array = {3, 1, 5, 0, 10, 3, 5, 9, 1, 4};
 
         printArray(array);
         quickSort(array);
         printArray(array);
-
     }
 
     public static void quickSort(int[] a) {
         quickSort(a, 0, a.length - 1);
     }
 
-    public static int partition(int[] a, int start, int end) {
+    public static void quickSort(int[] a, int start, int end) {
+        int part = partition(a, start, end);
+        if(start < part - 1) {
+            quickSort(a, start, part - 1);
+        }
+        if(part < end) {
+            quickSort(a, part, end);
+        }
+    }
 
+    public static int partition(int[]a, int start, int end) {
         int pivot = a[(start + end) / 2];
 
-        while (start <= end) {
-            while (a[start] < pivot) start++;
-            while (a[end] > pivot) end--;
-
-            if (start <= end) {
+        while(start <= end) {
+            while(a[start] < pivot) start++;
+            while(a[end] > pivot) end--;
+            if(start <= end) {
                 swap(a, start, end);
                 start++;
                 end--;
@@ -37,23 +42,9 @@ public class QuickSort {
     }
 
     public static void swap(int[] a, int start, int end) {
-        int temp;
-        temp = a[start];
+        int temp = a[start];
         a[start] = a[end];
         a[end] = temp;
-    }
-
-
-    public static void quickSort(int[] a, int start, int end) {
-
-        int part2 = partition(a, start, end);
-
-        if (start < part2 - 1) {
-            quickSort(a, start, part2 - 1);
-        }
-        if (start < end) {
-            quickSort(a, part2, end);
-        }
     }
 
     public static void printArray(int[] a) {
