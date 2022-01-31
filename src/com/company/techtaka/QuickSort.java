d5ebdb26adca4f3edacdcab7c3db2a4557bda8f3
JihoKevin <jhshin925@gmail.com>
2022-01-31 13:39:07 +0900

    Quick SOrt

--- src/com/company/techtaka/QuickSort.java
+++ src/com/company/techtaka/QuickSort.java
@@ -11,6 +11,42 @@ public class QuickSort {
         printArray(array);
     }
 
+//    public static void quickSort(int[] a) {
+//        quickSort(a, 0, a.length - 1);
+//    }
+//
+//    public static void quickSort(int[] a, int start, int end) {
+//        int part = partition(a, start, end);
+//        if(start < part - 1) {
+//            quickSort(a, start, part - 1);
+//        }
+//        if(part < end) {
+//            quickSort(a, part, end);
+//        }
+//    }
+//
+//    public static int partition(int[]a, int start, int end) {
+//        int pivot = a[(start + end) / 2];
+//
+//        while(start <= end) {
+//            while(a[start] < pivot) start++;
+//            while(a[end] > pivot) end--;
+//            if(start <= end) {
+//                swap(a, start, end);
+//                start++;
+//                end--;
+//            }
+//        }
+//
+//        return start;
+//    }
+//
+//    public static void swap(int[] a, int start, int end) {
+//        int temp = a[start];
+//        a[start] = a[end];
+//        a[end] = temp;
+//    }
+
     public static void quickSort(int[] a) {
         quickSort(a, 0, a.length - 1);
     }
@@ -25,9 +61,8 @@ public class QuickSort {
         }
     }
 
-    public static int partition(int[]a, int start, int end) {
+    public static int partition(int[] a, int start, int end) {
         int pivot = a[(start + end) / 2];
-
         while(start <= end) {
             while(a[start] < pivot) start++;
             while(a[end] > pivot) end--;
@@ -37,7 +72,6 @@ public class QuickSort {
                 end--;
             }
         }
-
         return start;
     }
 
