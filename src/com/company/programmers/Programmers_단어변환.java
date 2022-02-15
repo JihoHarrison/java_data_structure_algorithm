9596e98e0c4b8f863160d2a0aadaa170ccc8207e
JihoKevin <jhshin925@gmail.com>
2022-02-15 17:07:51 +0900

    Programmers 단어변환 revise

--- src/com/company/programmers/Programmers_단어변환.java
+++ src/com/company/programmers/Programmers_단어변환.java
@@ -5,6 +5,7 @@ package com.company.programmers;
  * User: sinjiho
  * Date: 2022/02/15
  * Time: 1:42 오후
+ * desc: 걍 문제 조건이 명확하지 않은게 조오금 이상한듯..?
  */
 public class Programmers_단어변환 {
 
@@ -22,28 +23,25 @@ public class Programmers_단어변환 {
     public static int solution(String begin, String target, String[] words) {
 
         boolean[] visited = new boolean[words.length];
+        // 단어가 한 번도 교체되지 않을 경우를 판별
+        answer = 100;
+        System.out.println(words.length);
 
         dfs(begin, target, words, visited, 0);
 
         System.out.println(answer);
-
-        return answer;
+        System.out.println(words.length);
+        return answer == 100 ? 0 : answer;
     }
 
     public static void dfs(String begin, String target, String[] words, boolean[] visited, int count) {
 
         if (target.equals(begin)) {
-            answer = count;
             answer = Math.min(answer, count);
             return;
         }
 
         for (int i = 0; i < words.length; i++) {
-
-            if (visited[i]) {
-                continue;
-            }
-
             int k = 0;
             for (int j = 0; j < begin.length(); j++) {
                 if (begin.charAt(j) == words[i].charAt(j)) {
@@ -52,16 +50,12 @@ public class Programmers_단어변환 {
             }
 
             if (k == begin.length() - 1) {
-                visited[i] = true;
-                dfs(words[i], target, words, visited, count + 1);
-                visited[i] = false;
+                if (!visited[i]) {
+                    visited[i] = true;
+                    dfs(words[i], target, words, visited, count + 1);
+                    visited[i] = false;
+                }
             }
-
-
         }
-
-
     }
-
-
 }
