2c38a1d90d49f96366146def49275e71cd6ec99a
JihoKevin <jhshin925@gmail.com>
2022-02-16 23:13:28 +0900

    Programmers_괄호변환 unsolved

---
+++ src/com/company/programmers/Programmers_괄호변환.java
@@ -0,0 +1,57 @@
+package com.company.programmers;
+
+/**
+ * Created by JihoKevin.
+ * User: sinjiho
+ * Date: 2022/02/16
+ * Time: 8:30 오후
+ */
+public class Programmers_괄호변환 {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static String solution(String p) {
+        String answer = "";
+
+
+        return answer;
+    }
+
+
+//    public static String split(String w) {
+//
+//        if(w.length() == 0) return "";
+//
+//        StringBuilder u = new StringBuilder();
+//        StringBuilder v = new StringBuilder();
+//
+//    }
+//
+//    public static boolean isGood(String str) {
+//
+//        int open = 0;
+//
+//        if (str.charAt(0) == ')') return false;
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == '(') {
+//                open++;
+//            } else {
+//                open--;
+//            }
+//            if (open < 0) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//
+//    }
+
+
+}
