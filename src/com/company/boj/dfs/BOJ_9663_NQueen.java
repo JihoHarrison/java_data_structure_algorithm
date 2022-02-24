c2621888bbe8b41e017a0f2875b7732068503a28
JihoKevin <jhshin925@gmail.com>
2022-02-24 20:59:49 +0900

    BOJ_9663_NQeen

--- src/com/company/boj/dfs/BOJ_9663_NQueen.java
+++ src/com/company/boj/dfs/BOJ_9663_NQueen.java
@@ -1,5 +1,7 @@
 package com.company.boj.dfs;
 
+import java.util.Scanner;
+
 /**
  * Created by JihoKevin.
  * User: sinjiho
@@ -8,15 +10,50 @@ package com.company.boj.dfs;
  */
 public class BOJ_9663_NQueen {
 
+    private static int n;
+    private static int[] queen;
+    private static int count = 0;
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        queen = new int[n];
+
+        dfs(0);
+        System.out.println(count);
+
     }
 
-    public static void dfs() {
+    public static void dfs(int row) {
 
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            queen[row] = i;
+
+            if (check(row)) {
+                dfs(row + 1);
+            }
+        }
     }
 
+    public static boolean check(int col) {
 
+        for (int i = 0; i < col; i++) {
+            if (queen[col] == queen[i]) {
+                return false;
+            }
 
+            if (Math.abs(col - i) == Math.abs(queen[i] - queen[col])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
