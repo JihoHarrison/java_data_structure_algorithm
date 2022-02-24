3d9529a31e7f3424801ec9036f53e644e383f8fd
JihoKevin <jhshin925@gmail.com>
2022-02-24 19:48:21 +0900

    BOJ_9663_NQeen

---
+++ src/com/company/boj/dfs/BOJ_9663_NQueen.java
@@ -0,0 +1,22 @@
+package com.company.boj.dfs;
+
+/**
+ * Created by JihoKevin.
+ * User: sinjiho
+ * Date: 2022/02/24
+ * Time: 7:26 오후
+ */
+public class BOJ_9663_NQueen {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void dfs() {
+
+    }
+
+
+
+}
