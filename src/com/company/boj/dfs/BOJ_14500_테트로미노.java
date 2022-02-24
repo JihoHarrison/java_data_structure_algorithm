722ea058990b896f43a9650e4c9c202cef5478e6
JihoKevin <jhshin925@gmail.com>
2022-02-24 16:31:39 +0900

    테트로미노 풀기 전

---
+++ src/com/company/boj/dfs/BOJ_14500_테트로미노.java
@@ -0,0 +1,26 @@
+package com.company.boj.dfs;
+
+/**
+ * Created by JihoKevin.
+ * User: sinjiho
+ * Date: 2022/02/23
+ * Time: 9:59 오후
+ */
+public class BOJ_14500_테트로미노 {
+
+    
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+
+    private static void dfs() {
+
+    }
+
+
+
+}
