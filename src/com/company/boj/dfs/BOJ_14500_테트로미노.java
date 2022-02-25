6a8bcb525ae748b1c92a50897e81e6c48b0a572a
JihoKevin <jhshin925@gmail.com>
2022-02-25 16:59:48 +0900

    Solving BOJ_14500_테트로미노

--- src/com/company/boj/dfs/BOJ_14500_테트로미노.java
+++ src/com/company/boj/dfs/BOJ_14500_테트로미노.java
@@ -1,5 +1,7 @@
 package com.company.boj.dfs;
 
+import java.util.Scanner;
+
 /**
  * Created by JihoKevin.
  * User: sinjiho
@@ -8,19 +10,31 @@ package com.company.boj.dfs;
  */
 public class BOJ_14500_테트로미노 {
 
-    
+    private static int n;
+    private static int m;
+    private static int[][] map;
 
     public static void main(String[] args) {
-        
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        n = scan.nextInt();
+        m = scan.nextInt();
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                map[i][j] = scan.nextInt();
+            }
+        }
+
+
 
     }
-
 
     private static void dfs() {
 
+
+
     }
-
-
-
 }
